e10d89cd9d20522d160ff623e06b9e508fa4bcd9
ypradafah28 <47551707+ypradafah28@users.noreply.github.com>
2022-11-09 15:31:09 +0700

    fix bug navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,37 +1,42 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faListNumeric, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar() {
 
     const [barClick, setBarClick] = useState(false)
     const [closeClick, setCloseClick] = useState(true)
     const [imgView, setImgView] = useState(false)
-    const [list, setList] = useState(false)
-
+    const [list, setList] = useState(true)
 
     // const barRef = useRef()
     // // const closeRef = useRef()
-    // // const listRef = useRef()
+    // const listRef = useRef()
     // const imgRef = useRef()
 
-
+    // useEffect(()=>{
+    //     setList(true)
+    // })
     const barHandler = () => {
         setBarClick(!barClick)
         setCloseClick(!closeClick)
         setImgView(!imgView)
-        setList(!list)
+        setList(false)
         // barRef.current.classList.add("hidden")
-        // closeRef.current.classList.remove("hidden")
+        // listRef.current.classList.remove("hidden")
         // imgRef.current.classList.add('translate-x-[10rem]')
     }
 
     const closeHandler = () => {
+
         setBarClick(!barClick)
         setCloseClick(!closeClick)
         setImgView(!imgView)
+        // setList(false)
         // closeRef.current.classList.add("hidden")
     }
+
+
     // const barHandler = () => {
     //     const list = document.getElementById('list')
     //     const bar = document.getElementById('bar')
@@ -58,18 +63,18 @@ function Navbar() {
 
     return (
         <nav>
-            <section>
+            <section className='h-full'>
                 <figure className='absolute z-10 top-0 left-0 w-full' >
-                    <img id='img' className={`w-full object-cover transition ease-in-out delay-[15ms] duration-[850ms] ${imgView ? " translate-x-[10rem] " : ""}`} src="./bunaken.png" alt="" />
+                    <img id='img' className={`w-full transition ease-in-out delay-[15ms] duration-[850ms] ${imgView ? " translate-x-[10rem] " : ""}`} src="./bunaken.png" alt="" />
                 </figure>
                 <section className=' relative z-20 flex justify-end top-12 right-20 cursor-pointer '>
                     <FontAwesomeIcon id='bar' onClick={barHandler} className={`text-white h-6 ${barClick ? 'hidden' : 'visible'} `} icon={faBars} />
                 </section>
                 <section className=' relative z-20 flex justify-end top-12 right-20 cursor-pointer'>
-                    <FontAwesomeIcon onClick={closeHandler} className={` text-white h-6 ${ closeClick ? "hidden" : "visible" } `} icon={faXmark} />
+                    <FontAwesomeIcon onClick={closeHandler} className={` text-white h-6 ${closeClick ? "hidden" : "visible"} `} icon={faXmark} />
                 </section>
             </section>
-            <section id='list' className={`h-full w-32 py-10 px-8 absolute top-0 bg-white`}>
+            <section id='list' className={`h-full w-32 py-10 z-0 px-8 absolute top-0 bg-white ${list ? "hidden" : ""}`}>
                 <ul className='font-sans text-base'>
                     <li className='my-4'><a href="/#"> Home </a></li>
                     <li className='my-4'><a href="/#"> Services </a></li>
